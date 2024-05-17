e90bc0179e86eb2217d2be4f66847b9d2c279c93
yasaman <101281998+yasamanBD@users.noreply.github.com>
2024-05-17 22:10:07 +0330

    Add files via upload

---
+++ MUTEX/MUTEX/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace MUTEX
+{
+    class Program
+    {
+        static Mutex mutex = new Mutex(); // Mutex object
+
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                Thread thread = new Thread(WithdrawMoney);
+                thread.Start(i + 1);
+            }
+            Console.ReadKey();
+        }
+
+        static void WithdrawMoney(object accountId)
+        {
+            mutex.WaitOne(); // Entering critical section
+            try
+            {
+                Console.WriteLine($"Account {accountId} is withdrawing money...");
+                Thread.Sleep(2000); // Simulate withdrawal
+                Console.WriteLine($"Account {accountId} has withdrawn money.");
+            }
+            finally
+            {
+                mutex.ReleaseMutex(); // Exiting critical section
+            }
+        }
+    }
+}
